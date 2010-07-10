d839c5552dd33d74eb1f3c908bc911b1a51795e2
Gabriel Dunne <gdunne@quilime.com>
2010-07-09 18:26:55 -0700

    replaced tabs with spaces

--- public/index.php
+++ public/index.php
@@ -4,40 +4,40 @@
 
     $url_parts = get_url();
 
-	list($response_format, $response_mime_type) = parse_format($url_parts['extension'], 'html');	
-	
-	# setup template
-	$t = get_template_instance();	
-	$t->response_format = $response_format;
-	$t->assign('view', $_GET['v']);
-		
-	# is folder
-	if (is_dir(LOCAL_ROOT . CONTENT_DIR . $url_parts['url']) && $url_parts['url'] != "/") {
-		list($data, $total) = get_entries($url_parts['url']);
-		$t->assign('page_title', preg_replace('{^/|/$}', '', $url_parts['url']));		
-		$t->assign('data', $data);		
-	}
-	# is file
-	else if (is_file( LOCAL_ROOT . CONTENT_DIR . $url_parts['url'])) {
-		$t->assign('single', true);		
-		$t->assign('data', parse_entry(new SplFileInfo(LOCAL_ROOT . CONTENT_DIR . $url_parts['url'])));
-		$template = 'single.'.$response_format.'.tpl';		
-	}
-	# is page
-	else if (is_file( LOCAL_ROOT . PAGE_DIR . $url_parts['url'])) {
-		$page = parse_entry(new SplFileInfo(LOCAL_ROOT . PAGE_DIR . $url_parts['url']), 1);
-		$t->assign('data', $page);
-		$template = $page['config']['template'] ? $page['config']['template'] . '.' . $response_format . '.tpl' : 'page.' . $response_format . '.tpl';
-	}
-	# default (all entries)
-	else {
-		list($data, $total) = get_entries();
-		$t->assign('data', $data);		
-	}
-	
-	# render
-	$t->assign('total', $total);	
+    list($response_format, $response_mime_type) = parse_format($url_parts['extension'], 'html');    
+    
+    # setup template
+    $t = get_template_instance();    
+    $t->response_format = $response_format;
+    $t->assign('view', $_GET['v']);
+        
+    # is folder
+    if (is_dir(LOCAL_ROOT . CONTENT_DIR . $url_parts['url']) && $url_parts['url'] != "/") {
+        list($data, $total) = get_entries($url_parts['url']);
+        $t->assign('page_title', preg_replace('{^/|/$}', '', $url_parts['url']));        
+        $t->assign('data', $data);        
+    }
+    # is file
+    else if (is_file( LOCAL_ROOT . CONTENT_DIR . $url_parts['url'])) {
+        $t->assign('single', true);        
+        $t->assign('data', parse_entry(new SplFileInfo(LOCAL_ROOT . CONTENT_DIR . $url_parts['url'])));
+        $template = 'single.'.$response_format.'.tpl';        
+    }
+    # is page
+    else if (is_file( LOCAL_ROOT . PAGE_DIR . $url_parts['url'])) {
+        $page = parse_entry(new SplFileInfo(LOCAL_ROOT . PAGE_DIR . $url_parts['url']), 1);
+        $t->assign('data', $page);
+        $template = $page['config']['template'] ? $page['config']['template'] . '.' . $response_format . '.tpl' : 'page.' . $response_format . '.tpl';
+    }
+    # default (all entries)
+    else {
+        list($data, $total) = get_entries();
+        $t->assign('data', $data);        
+    }
+    
+    # render
+    $t->assign('total', $total);    
     header("Content-Type: {$response_mime_type}; charset=UTF-8");
-	$t->render($template);
-	
+    $t->render($template);
+    
 ?>
